22279f51a7940e4504c63f4df83a0f2ea35166ab
Mayank <mayankkumarcse@gmail.com>
2024-09-04 20:33:30 +0530

    nodejs os

---
+++ os.js
@@ -0,0 +1,21 @@
+//  Node.js OS provides some basic operating-system related utility functions.
+
+const os = require('os');
+
+//  console.log("	os.uptime():",	os.uptime())
+
+// 1.	os.arch()	 
+// 2.	os.cpus()   
+// 3.	os.endianness() 
+// 4.	os.freemem()	 
+// 5.	os.homedir()	 
+// 6.	os.hostname() 
+// 7.	os.loadavg() 
+// 8.	os.networkinterfaces()	 
+// 9.	os.platform()	 
+// 10.	os.release()	 
+// 11.	os.tmpdir()	 
+// 12.	os.totalmem()	 
+// 13.	os.type() 
+// 14.	os.uptime()	 
+// 15.	os.userinfo([options])	 
